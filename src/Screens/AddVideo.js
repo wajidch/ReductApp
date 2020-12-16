f6e6f0681bb378304100782a0f6b5c658c08b08b
decoreitsol <decoreitsol>
2020-12-16 21:35:47 +0500

    upload api

--- src/Screens/AddVideo.js
+++ src/Screens/AddVideo.js
@@ -2,43 +2,84 @@ import React, { Component } from 'react';
 import '../App.css';
 import { Nav, SideDrawer } from '../Components'
 import image from '../img.jpg'
+import axios from 'axios';
+let filename;
 class VideoScreen extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      shown: true,
-      videoData: []
+      file:''
     };
     this.setShown = this.setShown.bind(this);
+    
   }
   setShown(value) {
     this.setState({ shown: value })
   }
   componentDidMount() {
+   
+  }
+  onChangeFile(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    var file = event.target.files[0];
+    filename=file.name
+    console.log(filename);
+
     var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
-    var urlencoded = new URLSearchParams();
-    urlencoded.append("userid", "A3937168");
-
+    const data = new FormData();
+    data.append("userid", "48886ACD");
+    data.append("file",file);
+    // urlencoded.append();
     var requestOptions = {
       method: 'POST',
       headers: myHeaders,
-      body: urlencoded,
+      body: data,
       redirect: 'follow'
     };
 
-    fetch("https://aws-dev.xcellence.tech/vr_api/redact/get_video_list", requestOptions)
+    fetch("https://aws-dec.xcellence.tech/vr_api/redact/video_redact", requestOptions)
       .then(response => response.text())
       .then(result => {
         let data = JSON.parse(result)
-        this.setState({ videoData: data.message })
+        console.log("data",data)
+       
       })
       .catch(error => console.log('error', error));
-  }
+
+}
+onDownloadFile(){
+ 
+
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+  var urlencoded = new URLSearchParams();
+  urlencoded.append("userid", "48886ACD");
+  urlencoded.append("filename", "IMG_8222_redact.mp4");
+
+  var requestOptions = {
+   
+    headers: myHeaders,
+    body: urlencoded,
+  };
+
+ 
+
+  
+  axios.get(`https://aws-dec.xcellence.tech/vr_api/redact/download_result`,{
+    requestOptions
+})
+      .then(res => {
+        const data = res;
+
+        console.log("data",data);
+            })
+}
   render() {
-    console.log(this.state.videoData)
     return (
       <div className="App">
         <SideDrawer shown={this.state.shown} />
@@ -51,9 +92,9 @@ class VideoScreen extends Component {
           <h1>  Upload Video </h1>
           <p> Test File </p>
           <form class="addVideoForm"> 
-            <input type="text" placeholder="Click to select video for recognition" class="videoInput" />
-            <button class="uploadBtn" > UPLOAD TEST FILE FOR RECOGHITION </button>
-            <button class="resultBtn"> DOWNLOAD RESULT AS JSON </button>
+            <input type="file" id="file" onChange={this.onChangeFile.bind(this)} placeholder="Click to select video for recognition" className="videoInput" />
+            {/* <button className="uploadBtn" > UPLOAD TEST FILE FOR RECOGHITION </button> */}
+            <button type="button" className="resultBtn" onClick={this.onDownloadFile.bind()}> DOWNLOAD RESULT AS JSON </button>
           </form>
           <br />
         </div>
